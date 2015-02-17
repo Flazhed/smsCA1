791fcc576a0ba671e02e270b65c4a3997e42214e
Schultzz <ms.fuge@gmail.com>
2015-02-17 09:40:59 +0100

    Tilføjet protokollen

---
+++ smsCA1/src/shared/ProtocolStrings.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared;
+
+/**
+ *
+ * @author MS
+ */
+public class ProtocolStrings {
+
+    public static final String STOP = "##STOP##";
+    public static final String message = "MESSAGE";
+    public static final String connect = "CONNECT";
+    public static final String send = "SEND";
+    public static final String close = "CLOSE";
+    public static final String online = "ONLINE";
+    public static final String separator = "#";
+}
