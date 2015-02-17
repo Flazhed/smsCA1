c8dea2866230ddfecb4b7ec4ef5d21cf9d0f4331
Flazhed <soren1337@gmail.com>
2015-02-17 10:56:18 +0100

    Client added

---
+++ smsCA1/src/chatclient/ClientObserver.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatclient;
+
+/**
+ *
+ * @author Søren
+ */
+public interface ClientObserver {
+    
+    void messageArrived(String mes);
+    void usersUpdated(String users);
+    
+}
