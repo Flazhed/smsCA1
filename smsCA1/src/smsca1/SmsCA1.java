30a1dcfc8ea2501c86950d2a6932cafe800a55b9
Flazhed <soren1337@gmail.com>
2015-02-17 08:54:21 +0100

    First commit, fellas!

---
+++ smsCA1/src/smsca1/SmsCA1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smsca1;
+
+/**
+ *
+ * @author Søren
+ */
+public class SmsCA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("WE ON TV!!!!");
+    }
+    
+}
